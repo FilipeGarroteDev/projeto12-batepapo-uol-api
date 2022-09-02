8a51302d17dff43a9b255a71c30b9e0f0f1cd1dc
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-02 15:06:04 -0300

    feat(envio de mensagem) - Realizado o envio de mensagens ao servidor (post /messages), com validação de erros via joi

--- src/server.js
+++ src/server.js
@@ -24,10 +24,16 @@ const userSchema = joi.object({
   name: joi.string().required(),
 });
 
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid('message', 'private_message'),
+});
+
 server.post('/participants', async (req, res) => {
   const { name } = req.body;
   const validation = userSchema.validate(req.body);
-  const users = await db.collection('users').find().toArray();
+  const hasUser = await db.collection('users').findOne({ name: req.body.name });
 
   if (validation.error) {
     return res
@@ -36,7 +42,7 @@ server.post('/participants', async (req, res) => {
         'O nome de usuário é obrigatório. Por favor, insira um nome válido! :)'
       );
   }
-  if (users.find((value) => value.name === name)) {
+  if (hasUser) {
     return res
       .status(409)
       .send(
@@ -44,7 +50,7 @@ server.post('/participants', async (req, res) => {
       );
   }
   try {
-    await db.collection('users').insertOne({
+    await db.collection().insertOne({
       name,
       lastStatus: Date.now(),
     });
@@ -58,5 +64,69 @@ server.get('/participants', async (req, res) => {
   const users = await db.collection('users').find().toArray();
   return res.send(users);
 });
+
+server.post('/messages', async (req, res) => {
+  let message = req.body;
+  const { user } = req.headers;
+  const activeUser = await db.collection('users').findOne({ name: user });
+  const validation = messageSchema.validate(message);
+
+  if (!activeUser || validation.error) {
+    return res.sendStatus(422);
+  }
+
+  message = {
+    ...message,
+    time: dayjs(Date.now()).format('hh:mm:ss'),
+  };
+  await db.collection('messages').insertOne(message);
+  return res.sendStatus(201);
+});
+
+// server.post('/messages', (req, res) => {
+//   const { to, text, type } = req.body;
+//   const { user: from } = req.headers;
+//   const loggedUser = users.find((user) => user.name === from);
+
+//   if (to === '' || text === '') {
+//     console.log('to');
+//     res.status(422).send('Você não pode mandar uma mensagem vazia.');
+//     return;
+//   }
+
+//   if (type !== 'message' && type !== 'private_message') {
+//     console.log('message');
+//     res.sendStatus(422);
+//     return;
+//   }
+
+//   if (loggedUser) {
+//     console.log('logged');
+//     res.sendStatus(422);
+//     return;
+//   }
+
+//   const mensagem = {
+//     to,
+//     text,
+//     type,
+//     from,
+//     time: formatado,
+//   };
+
+//   messages.push(mensagem);
+//   res.send(messages);
+// });
+
+// server.get('/messages', (req, res) => {
+//   const { limit } = req.query;
+//   let lastMessages = messages;
+
+//   if (limit) {
+//     lastMessages = messages.slice(-limit);
+//   }
+
+//   res.send(lastMessages);
+// });
 
 server.listen(5000, () => console.log('Listening on port 5000'));
