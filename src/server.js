1a9c7715a489625a54007ba7edf5cc2bb9d41c53
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-02 12:36:53 -0300

    feat(armazenamento usuário) - Realizado o armazenamento (post '/participants') de usuário no banco de dados, com a tratativa de erros, via joi

--- src/server.js
+++ src/server.js
@@ -20,4 +20,38 @@ mongoClient.connect().then(() => {
   db = mongoClient.db('batepapouol');
 });
 
+const userSchema = joi.object({
+  name: joi.string().required(),
+});
+
+server.post('/participants', async (req, res) => {
+  const { name } = req.body;
+  const validation = userSchema.validate(req.body);
+  const users = await db.collection('users').find().toArray();
+
+  if (validation.error) {
+    return res
+      .status(422)
+      .send(
+        'O nome de usuário é obrigatório. Por favor, insira um nome válido! :)'
+      );
+  }
+  if (users.find((value) => value.name === name)) {
+    return res
+      .status(409)
+      .send(
+        'Esse nome de usuário já está sendo utilizado. Por favor, escolha outro nome! :)'
+      );
+  }
+  try {
+    await db.collection('users').insertOne({
+      name,
+      lastStatus: Date.now(),
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
 server.listen(5000, () => console.log('Listening on port 5000'));
