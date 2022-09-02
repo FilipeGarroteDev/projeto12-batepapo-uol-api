0c3a6cebf7431372b07bf0b34771ccbe32e8b9c8
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-02 12:03:12 -0300

    feat(conexão mongodb) - Realizada a conexão ao banco de dados, com variável de ambiente

--- src/server.js
+++ src/server.js
@@ -3,9 +3,21 @@
 import dayjs from 'dayjs';
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import joi from 'joi';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db('batepapouol');
+});
 
 server.listen(5000, () => console.log('Listening on port 5000'));
